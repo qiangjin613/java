dfb9e96e4743233cdb54ed3b9746dd6de69c191a
QiangJin <1291491915@qq.com>
2021-04-12 22:01:09 +0800

    异常

--- chapter_15_异常/TryWithResources用法.java
+++ chapter_15_异常/TryWithResources用法.java
@@ -96,15 +96,16 @@ class StreamsAreAutoCloseable {
 /**
  * 【揭示细节】
  * 创建自己的 AutoCloseable 类
- *  try-with-resources 定义子句中创建的对象（在括号内）必须实现 java.lang.AutoCloseable 接口，这个接口只有一个方法：close()
+ * try-with-resources 定义子句中创建的对象（在括号内）必须实现 java.lang.AutoCloseable 接口，这个接口只有一个方法：close()
  */
 class Reporter implements AutoCloseable {
     String name = getClass().getSimpleName();
     Reporter () {
+        // 输出运行时类名
         System.out.println("Creating " + name);
     }
     @Override
-    public void close() throws Exception {
+    public void close() {
         System.out.println("Closing " + name);
     }
 }
@@ -117,8 +118,6 @@ class AutoCloseableDetails {
                 Second s = new Second()
         ) {
 
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
@@ -138,11 +137,11 @@ class AutoCloseableDetails {
 //}
 
 /**
- * 构造函数抛出异常的情况
+ * [1] 如果构造函数抛出异常
  */
 class CE extends Exception {}
-class SecondException extends Reporter {
-    SecondException() throws CE {
+class SecondExcept extends Reporter {
+    SecondExcept() throws CE {
         super();
         throw new CE();
     }
@@ -151,14 +150,74 @@ class ConstructorException {
     public static void main(String[] args) {
         try(
                 First f = new First();
-                SecondException s = new SecondException();
+                // SecondExcept 抛出异常，Second 和 try 的主体将不会执行，
+                // 然后调用 f.close()，s.close() 不会执行（因为 SecondExcept 未成功创建）
+                SecondExcept s = new SecondExcept();
                 Second s2 = new Second()
         ) {
             System.out.println("In body");
         } catch (CE e) {
             System.out.println("Caught CE: " + e);
-        } catch (Exception e) {
-            System.out.println("Caught Exception: " + e);
+        }
+    }
+}
+
+/**
+ * [2] 如果 try 主体抛出异常
+ */
+class Third extends Reporter {}
+class BodyException {
+    public static void main(String[] args) {
+        try(
+                First f = new First();
+                Second s = new Second()
+        ) {
+            System.out.println("In body");
+            Third t = new Third();
+            // 这里抛出异常，下面的语句将不会执行。
+            // 同时，SecondExcept 对象将永远不会被清除，因为它不是在资源规范头中创建的，所以它没有被保护！
+            new SecondExcept();
+            System.out.println("End of body");
+        } catch (CE e) {
+            System.out.println("Caught CE: " + e);
+        }
+    }
+}
+
+/**
+ * [3] 如果 close() 抛出异常
+ */
+class CloseException extends Exception {}
+class Reporter2 implements AutoCloseable {
+    String name = getClass().getSimpleName();
+    Reporter2() {
+        // 输出运行时类名
+        System.out.println("Creating " + name);
+    }
+    @Override
+    public void close() throws CloseException {
+        System.out.println("Closing " + name);
+    }
+}
+class Closer extends Reporter2 {
+    @Override
+    public void close() throws CloseException {
+        super.close();
+        throw new CloseException();
+    }
+}
+class CloseExceptions {
+    public static void main(String[] args) {
+        try(
+                First f = new First();
+                Closer c = new Closer();
+                Second s = new Second()
+        ) {
+            System.out.println("In body");
+        }
+        // 处理关闭时抛出的异常
+        catch (CloseException e) {
+            System.out.println("Caught: " + e);
         }
     }
 }
--- chapter_15_异常/一些零碎的知识点.java
+++ chapter_15_异常/一些零碎的知识点.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * 【特例：RuntimeException】
  * 代码中只有 RuntimeException（及其子类）类型的异常可以被忽略，因为编译器强制要求处理所有受检查类型的异常
@@ -21,7 +24,9 @@ class NeverCaught {
 class IgnoredFinally  {
     public static void f() throws NullPointerException, ArrayIndexOutOfBoundsException {
         if (1 == 2) {
-            try { }
+            try {
+
+            }
             // try 块没有执行，所以 finally 也就没有执行
             finally {
                 System.out.println("11111111111111111");
@@ -31,5 +36,97 @@ class IgnoredFinally  {
 
     public static void main(String[] args) {
         f();
+    }
+}
+
+/**
+ * 【异常匹配】
+ * “最近”匹配、匹配派生类
+ */
+class Annoyance extends Exception {}
+class Sneeze extends Annoyance {}
+class Human {
+    public static void main(String[] args) {
+        // [1] 最近匹配
+        try {
+            throw new Sneeze();
+        } catch (Sneeze s) {
+            System.out.println("Caught Sneeze");
+        } catch (Annoyance a) {
+            System.out.println("Caught Annoyance");
+        }
+
+        // [2] 匹配派生类
+        try {
+            throw new Sneeze();
+        } catch (Annoyance a) {
+            System.out.println("Caught Annoyance");
+        }
+
+        // [3] 被“屏蔽”的派生类，会产生编译错误
+//        try {
+//            throw new Sneeze();
+//        } catch (Annoyance a) {
+//            System.out.println("Caught Annoyance");
+//        } catch (Sneeze s) {
+//            System.out.println("Caught Sneeze");
+//        }
+    }
+}
+
+/**
+ * 【把异常传递给控制台】
+ */
+class MainException {
+    public static void main(String[] args) throws IOException {
+        throw new IOException();
+    }
+}
+
+/**
+ * 【把“被检查的异常”转换为“不检查的异常”】
+ */
+class WrapCheckedException {
+    void throwRuntimeException(int type) {
+        try {
+            switch (type) {
+                case 0: throw new FileNotFoundException();
+                case 1: throw new IOException();
+                case 2: throw new RuntimeException("Where am I?");
+                default: return;
+            }
+        } catch (IOException | RuntimeException e) {
+            // 转换为“不检查的异常”，同时使用异常链保存异常信息
+            throw new RuntimeException(e);
+        }
+    }
+}
+class SomeOtherException extends Exception {}
+class TurnOffChecking {
+    public static void main(String[] args) {
+        WrapCheckedException wce = new WrapCheckedException();
+        wce.throwRuntimeException(3);
+        for (int i = 0; i < 4; i++) {
+            try {
+                if (i < 3) {
+                    wce.throwRuntimeException(i);
+                } else {
+                    throw new SomeOtherException();
+                }
+            } catch (SomeOtherException e) {
+                System.out.println("SomeOtherException: " + e);
+            } catch (RuntimeException re) {
+                // 把原先的那个异常给提取出来，然后就可以用它们自己的 catch 子句进行处理
+                try {
+                    throw re.getCause();
+                } catch (FileNotFoundException e) {
+                    System.out.println("FileNotFoundException: " + e);
+                } catch (IOException e) {
+                    System.out.println("IOException: " + e);
+                } catch (Throwable e) {
+                    System.out.println("Throwable: " + e);
+                }
+            }
+        }
     }
 }
