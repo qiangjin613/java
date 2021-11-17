a368b96f44d168b69215fb1e8204a2ee69be2b6b
qiangjin613 <qiangjin613@163.com>
2021-11-17 21:42:38 +0800

    控制线程

--- 并发实战/多线程基础/basic/control/DaemonThread.java
+++ 并发实战/多线程基础/basic/control/DaemonThread.java
@@ -4,6 +4,30 @@ package basic.control;
  * 后台线程（Daemon Thread），又称为 守护线程 或 精灵线程。任务是为其他的线程提供服务。如：JVM 的 GC 线程。
  * 特点：1）前台线程死亡，后台线程自动死亡；2）前台线程创建的子线程默认是前台线程，后台线程创建的子线程默认是后台线程。
  */
-public class DaemonThread {
+public class DaemonThread extends Thread {
+    public DaemonThread(String name) {
+        super(name);
+    }
 
+    @Override
+    public void run() {
+        for (int i = 0; i < 1_000; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        DaemonThread daemonThread = new DaemonThread("后台线程");
+        daemonThread.setDaemon(true);
+        daemonThread.start();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+        /* 通过观察，当 main 线程死亡时，后台线程也随之死亡 */
+    }
 }
+/*
+Note:
+1. 如果要将某个线程设置为后台线程，必须要在该线程启动之前设置（即，setDaemon 在 start 之前），否则引发 IllegalThreadStateException 异常
+2. 前台线程死亡后，JVM 通知后台线程死亡，在这个之间，从接收指令到做出相应需要一定时间
+ */
