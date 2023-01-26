245e2b2e985b4c5b11facb9c50edca72c46a27c0
qiangjin <qiangjin613@163.com>
2023-01-26 22:29:34 +0800

    feat(io): 添加 FileInputStream 操作示例
    提交人: qiangj

---
+++ io/src/main/java/example/inputstream/FileInputStreamExample.java
@@ -0,0 +1,61 @@
+package example.inputstream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * java.io.FileInputStream 示例：使用 read 读取文件数据。
+ *
+ * @author  qiangj
+ */
+public class FileInputStreamExample {
+    public static void main(String[] args) {
+        readFile();
+        readFile2();
+    }
+
+    /**
+     * 使用 read() 单个字节读取文件数据。
+     * <p>
+     * NOTE：
+     * <ul>
+     * <li> 读取多字节的数据时，会出现乱码，比如中文
+     * <li> 因为是单个字节读取，所以效率比较低。（可使用 read(byte[] b) 读取，提高效率）
+     * </ul>
+     */
+    public static void readFile() {
+        try (FileInputStream fileInputStream = new FileInputStream("D:\\CodeRepositories\\java\\io\\src\\main\\java\\example\\inputstream\\FileInputStreamExample.java")) {
+            // 使用读取文件数据
+            int byteData;
+            while ((byteData = fileInputStream.read()) != -1) {
+                System.out.print((char) byteData);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 使用 read(byte b[]) 读取多个字节数据（byte[] 相当于是一个 Buffer）
+     * <p>
+     * NOTE：
+     * <ul>
+     * <li> 读取多字节的数据时，也会出现乱码。
+     *      即使使用了相同的字符集（如文件的读写都是 UTF-8），也会因缓冲区 byte[] 的长度问题，造成部分的数据乱码
+     *      （如：3 个中文是 9 个字符，从字节流中读取 8 个字符进行解码，必然会造成数据的乱码）
+     * </ul>
+     */
+    public static void readFile2() {
+        try (FileInputStream fileInputStream = new FileInputStream("D:\\CodeRepositories\\java\\io\\src\\main\\java\\example\\inputstream\\FileInputStreamExample.java")) {
+            // 使用读取文件数据
+            int byteDataLength;
+            // 一次读取 8 个字节
+            byte[] byteBufferArray = new byte[8];
+            while ((byteDataLength = fileInputStream.read(byteBufferArray)) != -1) {
+                System.out.print(new String(byteBufferArray, 0, byteDataLength));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
